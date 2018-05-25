795d0531a3d405e992216a86ce5e84e1ef1e1cc7
Incoherente <manueld380@gmail.com>
2018-05-26 01:53:20 +0200

    Second Commit. Just checking how commit actually works

--- Main.py
+++ Main.py
@@ -4,3 +4,8 @@ class Main:
         self.partida_ganada = False
         self.partida_perdida = False
 
+if __name__ == '__main__':
+
+    my_game = Main()
+    print("Partida ganada {}.".format(my_game.partida_ganada))
+    print("Partida perdida{}.".format(my_game.partida_perdida))
